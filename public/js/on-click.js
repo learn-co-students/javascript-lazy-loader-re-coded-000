07b6a4d0573ee5d92d765ad455903a0ff83c1459
dina aziz <dina.aziz100@yahoo.com>
2017-03-15 20:28:32 +0000

    Done.

--- public/js/on-click.js
+++ public/js/on-click.js
@@ -1,6 +1,11 @@
-"use strict";
+
+  "use strict";
 
 $(document).ready(function() {
   // add click listener here
   // it should call on fetchJSON()
+  $('#load-cars').click(function(e){
+  	fetchJSON();
+  })
+});
 });
